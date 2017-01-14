9d18c1f8e51c61fa6b5d1bc397145d0eb0fc4b24
charlie42 <oninfrared@gmail.com>
2017-01-14 14:15:56 +0100

    all basics done1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# puts 'SETTING UP DEFAULT USER LOGIN'
-# user = User.create! :email => '1@1.1', :password => '111111', :password_confirmation => '111111'
-# puts 'New user created: ' << user.email
-# user2 = User.create! :email => '2@2.2', :password => '222222', :password_confirmation => '222222'
-# puts 'New user created: ' << user2.email
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :email => '1@1.1', :password => '111111', :password_confirmation => '111111'
+puts 'New user created: ' << user.email
+user2 = User.create! :email => '2@2.2', :password => '222222', :password_confirmation => '222222'
+puts 'New user created: ' << user2.email
 
 puts 'movies'
 movie = Movie.create! :name => 'first movie', :rating => 20
