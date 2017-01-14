44b0b316fdf98cf3f8eee580d9f62a2e7a83b270
charlie42 <oninfrared@gmail.com>
2017-01-14 14:14:51 +0100

    all basics done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie
   include Mongoid::Document
   field :name, type: String
   field :rating, type: Float
+
+  validates :name, presence:true
+  validates :rating, presence:true
+  validates :rating, numericality: { only_integer: true, less_than: 11}
 end
